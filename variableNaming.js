cdbffadbc7a98fbb6af84c8546bafa38eca01ac2
utsav <75293077+up1512001@users.noreply.github.com>
2023-08-13 08:17:48 +0000

    basic

---
+++ variableNaming.js
@@ -0,0 +1,27 @@
+let x = "abc";
+// let 1x = "xyz"; wrong naming
+let _x = "pqr";
+let $x = "uts";
+// let x y = "vvv"; no space allowed
+
+console.log(x,_x,$x);
+
+// let let = "kdhkj"; reserved word not allows
+// let y% = "abc"; not valid
+// sconsole.log(y%);
+
+console.log("10" - "20");  // -10s
+
+
+// 9 - "5" => 4
+// typeof(null) => object
+//  typeof(undefined) => undefined
+
+// NaN => not a number
+console.log("hi" - "hello"); // NaN
+//  NaN is property of global object
+// variable of global scope
+
+
+console.log(Number.NaN === Number.NaN) // false
+
